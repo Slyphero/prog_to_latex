e0686f1a3212009e5bc6db112e42f84b1c05d28a
Raphaël Heng <raphael.heng2@gmail.com>
2023-12-04 20:19:23 +0100

    correction verify_brackets
    ajout de la verification des parentheses

--- string_treatment.py
+++ string_treatment.py
@@ -8,8 +8,13 @@ class Token:
     def verify_brackets(self):
         brackets_stacks = stack.Stack()
         for token in self.tokens:
-            if token == '[':
+            if token == '[' or token == '(':
                 brackets_stacks.stack(token)
+            elif token == ')':
+                if (not brackets_stacks.is_empty()) and (brackets_stacks.stack_top() == '('):
+                    brackets_stacks.unstack()
+                else:
+                    return False
             elif token == ']':
                 if (not brackets_stacks.is_empty()) and (brackets_stacks.stack_top() == '['): 
                     brackets_stacks.unstack()
